6f60ae8404f9b7b0e14403b6ecc8d6c3769aa105
inpyeong <jinipyung@gmail.com>
2020-09-07 01:07:35 +0900

    Add helloDto method

--- src/main/java/com/jojoldu/book/springboot/web/HelloController.java
+++ src/main/java/com/jojoldu/book/springboot/web/HelloController.java
@@ -1,6 +1,8 @@
 package com.jojoldu.book.springboot.web;
 
+import com.jojoldu.book.springboot.web.dto.HelloResponseDto;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController // @ResponseBody 를 각 메소드마다 선언한 것과 같다.
@@ -10,4 +12,11 @@ public class HelloController {
     public String hello() {
         return "hello";
     }
+
+    @GetMapping("/hello/dto")
+    public HelloResponseDto helloDto(@RequestParam("name") String name,
+                                     @RequestParam("amount") int amount) {
+        return new HelloResponseDto(name, amount);
+    }
+
 }
